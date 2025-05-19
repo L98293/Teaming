43a3a3b93c15c6acb30710913f74f665b3143f34
김경윤 <l982930009@gmail.com>
2025-05-19 23:31:53 +0900

    서버 기본 틀

---
+++ src/main/java/teaming/teaming/TeamingApplication.java
@@ -0,0 +1,13 @@
+package teaming.teaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamingApplication.class, args);
+    }
+
+}
