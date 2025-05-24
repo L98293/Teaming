f808340d2981ca6522e1650fe3c0e4d024c3a608
김연호 <kimfine000@gmail.com>
2025-05-24 21:35:08 +0900

    feat: 로그인 기능 추가

---
+++ src/main/java/teaming/teaming/domain/Member.java
@@ -0,0 +1,44 @@
+package teaming.teaming.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;   // 닉네임
+    private String email;
+    private String password;
+
+    @ElementCollection
+    private List<String> techStack;  // ex. ["Java", "React"]
+
+    @OneToMany(mappedBy = "leader")
+    private List<Project> myProjects = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "member_joined_project",
+            joinColumns = @JoinColumn(name = "member_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private List<Project> joinedProjects = new ArrayList<>(); // ✅ 요거!
+
+    public Member() {}
+
+    public Member(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
